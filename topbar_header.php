6f9a20738a65ae36688d625f662b723f3d1f0556
Robin Spiegel <robinspiegel@gmail.com>
2014-08-04 16:05:52 +0200

    Add contact information header bar

--- topbar_header.php
+++ topbar_header.php
@@ -1,11 +1,38 @@
-<header class="header clearfix">
+<div class="animationload"><div id="intro"></div></div>
+
+    <div class="wrapper">
+        <section class="topbar clearfix">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="social clearfix">
+                            <ul>
+                                <li><a data-placement="bottom" data-toggle="tooltip" data-original-title="Twitter" title="" href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a data-placement="bottom" data-toggle="tooltip" data-original-title="Linkedin" title="" href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a data-placement="bottom" data-toggle="tooltip" data-original-title="Skype" title="" href="#"><i class="fa fa-skype"></i></a></li>
+                            </ul>
+                        </div><!-- end social -->
+                    </div><!-- end col-lg-6 -->
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="callus clearfix">
+                            <ul>
+                                <li><p><span>Email :</span> service@insightyou.com</p></li>
+                                <li><p><span>Telefoon :</span> +31 (0)10 340 30 80</p></li>
+                            </ul>
+                        </div><!-- end callus -->
+                    </div><!-- end col-lg-6 -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end topbar -->
+
+        <header class="header clearfix">
 			<div class="container">
             	<div class="row">
                     <div class="col-lg-4 col-md-6 col-sm-12">
                        <div class="logo-wrapper clearfix">
 							<div class="logo">
 								<a href="index.php" title="Home">
-									<img src="images/logoiy.png" alt="Estate" width="200">
+									<img src="images/logo.png" alt="Estate">
 								</a>
 							</div><!-- /.site-name -->
 						</div><!-- /.logo-wrapper -->
