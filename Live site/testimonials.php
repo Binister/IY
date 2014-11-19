d17d16145142a6dbd6987a609c0f5b6d066202d4
Robin Spiegel <robinspiegel@gmail.com>
2014-11-19 15:15:55 +0100

    Add testimonials and pictures

---
+++ Live site/testimonials.php
@@ -0,0 +1,302 @@
+<?php
+
+
+   $testimonials = array(
+      '<div class="testi-container">
+      <blockquote>
+         <p>De uitslag van mijn InsightYou Sales profiel blijft me positief ‘achtervolgen’. Telkens als ik een gesprek voer met een klant zie ik mijn profiel terug in mijn handelen. Mijn ‘biologische kracht’ benut ik in mijn salesgesprekken en gebruik ik om mijn collega’s aan te vullen en te ondersteunen. Voor de punten waar ik onder de benchmark scoor roep ik actief hulp in. Mooi zou zijn om dit voor een salesteam te doen, zodat je optimaal van elkaar ‘gebruik’ kunt maken.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/wouter_glas.jpg" alt="" />
+   </div>
+
+   <div class="name">Wouter Glas
+      <br/>
+      <span class="label white radius">Manager &ndash; Pro Garden</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Het InsightYou Sales profiel heeft mij kennis laten maken met mijzelf. Ik kan mijn natuurlijke talenten nu effectief uitbouwen. Doen waar je goed in bent ervaar ik als plezier en energie gevend i.p.v. energie eisend. De “zwakke” punten in mijn profiel waren confronterend. Als sales professional ben je competitief en wil je de beste zijn. Hier nu bewust aan kunnen werken geeft mij zelfcontrole: ik weet immers waar mijn gedrag of acties vandaan komen.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/tim_de_vries.jpg" alt="" />
+   </div>
+
+   <div class="name">Tim de Vries 
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Het InsightYou profiel heeft mij een uniek inzicht gegeven in mijn commercië drives. Deze inzichten heb ik ook bij mijn sollicitatie gebruikt om de link te leggen tussen mij en mijn werkgever. Ik was hiermee sterk onderscheidend. Als sales professional kan ik met de verkregen inzichten mijn natuurlijke talenten beter inzetten, het maximale uit mezelf halen en mijn klanten nog beter te challengen. Het InsightYou profiel geeft echt inzicht in je eigen commercieel DNA.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/robert_kozijn.jpg" alt="" />
+   </div>
+
+   <div class="name">Robert Kozijn
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>De uitkomst mijn InsightYou Sales profiel was intrigerend door de unieke combinatie van DNA onderzoek en zelf-assessment. Het feedbackrapport geeft bijzondere inzichten en de uitleg is erg begrijpelijk. Tevens heeft het me enorm geïnspireerd. Ik heb het rapport tijdens mijn Year End Review besproken met mijn leidinggevende en samen hebben we gekeken hoe ik me komend jaar nog verder kan ontwikkelen als sales professional.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/jaap_van_dongen.jpg" alt="" />
+   </div>
+
+   <div class="name">Jaap van Dongen
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Het InsightYou Sales profiel heeft mij inzicht gegeven in mijn natuurlijke talenten en de ontwikkelingen die ik doorgemaakt heb. Dit inzicht heeft mij vertrouwen gegeven om te geloven in mijn talenten en daarmee de kans om die verder uit te bouwen. Tevens is het heel interessant om te zien hoe je scoort ten opzichte van je mede sales professionals. Het heeft mij een enorme ‘boost’ gegeven. Persoonlijk vind ik het profiel een ‘must’ als je je vak serieus neemt.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/anita_ilievski.jpg" alt="" />
+   </div>
+
+   <div class="name">Anita Ilievski
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Ik heb reeds een aantal waardevolle testen doorlopen die een goed beeld verschaffen van mij als persoon, en hoe anderen op mij kunnen reageren. Het InsightYou Sales profiel gaat dieper. Het onderscheidt zich door de focus op Sales en de concrete inzichten in mijn biologisch profiel d.m.v. DNA. Het gaf zo een duidelijk antwoord op mijn vragen, waardoor ik nu weet op welke wijze ik het maximale uit mijzelf kan halen.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/bas_smit.jpg" alt="" />
+   </div>
+
+   <div class="name">Bas Smit
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Hoe kun je anderen begrijpen, als je jezelf niet volledig begrijpt? Mijn InsightYou profiel biedt mij iedere dag de kans om als accountmanager te groeien. Ik zet het in om mijn sterke en zwakke punten te monitoren en te sturen. De inzichten deel ik o.a. met mijn manager, collega\'s en klanten. Dit heeft geresulteerd in (h)erkenning, openheid en diepgang binnen mijn contacten en heeft extra draagvlak gegeven voor mijn sales activiteiten!</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/danielle_zijlstra.jpg" alt="" />
+   </div>
+
+   <div class="name">Daniëlle Zijlstra
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Inzicht in je DNA zou ik willen omschrijven als de \'Beauty of the Brain\'. Het is zeer interessant om te zien dat de genen die een rol spelen bij mijn succes als sales professional al vanaf het begin van mijn bestaan hun werk doen. Om te kunnen ontwikkelen moet je weten waar je nu staat. Het InsightYou Sales profiel is die baseline, en helpt je niet alleen als individu, maar stelt je ook in staat beter samen te werken met klanten, partners en collega\'s.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/marco_hoekstra.jpg" alt="" />
+   </div>
+
+   <div class="name">Marco Hoekstra
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Mijn Sales profiel van InsightYou heeft mij zeer veel waardevolle informatie gegeven over hoe ik zaken aanpak, zowel intern als extern, en wat het effect daarvan is. Tevens zijn de verkregen inzichten zeer bruikbaar om klanten te analyseren en een persoonlijke aanpak te kiezen. Het feit dat de persoonlijke feedback gecombineerd wordt met een wetenschappelijke onderbouwing maakt mijn Sales profiel zeer uniek en bruikbaar!</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/pepijn_van_der_heijden.jpg" alt="" />
+   </div>
+
+   <div class="name">Pepijn van der Heijden
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Wie ben ik van nature op commercieel gebied? Hoe heb ik me ontwikkeld daarna? Hoe kan ik deze kennis gebruiken om mezelf commercieel te blijven ontwikkelen? Mijn commerciële DNA profiel heeft me veel zelfvertrouwen gegeven en laten zien welke kansen er allemaal voor mij liggen. Het rapport is ook erg waardevol geweest in sollicitatieprocedures. Het InsightYou profiel geeft echt inzicht in je eigen commercieel DNA.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/leanne_valom.jpg" alt="" />
+   </div>
+
+   <div class="name">Leanne Valom
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Bevestigd worden in mijn handelen. Begrijpen waarom activiteiten me energie kosten of geven. Inzien hoe ervaringen mijn gevoel, doen en laten bëinvloeden. Mijn InsightYou profiel geeft mij handvatten om de impact van negatieve ervaringen te reduceren en mijn positieve energie op peil te houden. Fascinerend zelfinzicht! Waardevol voor mijn toekomst als ondernemer. Het is verrijkend om bewust bekwaam en bewust onbekwaam te zijn.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/joel_van_amerongen.jpg" alt="" />
+   </div>
+
+   <div class="name">Joël van Amerongen
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Door het InsightYou Sales profiel weet ik exact waar mijn sterke punten liggen. Deze biologische inzichten heb ik in mijn sollicitatie gebruikt. Hierdoor heb ik niet alleen mijzelf weten uit te dagen, maar vooral mijn gesprekspartners. Mijn DNA profiel ‘matchte’ namelijk sterk met dat van mijn nieuwe werkgever. De volgende stap is om mijn nieuwe collega’s bewust te maken van hun biologische talent.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/joost_boelens.jpg" alt="" />
+   </div>
+
+   <div class="name">Joost Boelens
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Mijn InsightYou Sales profiel helpt mij mijn sterke saleseigenschappen verder uit te bouwen en zo onderscheidend te zijn. Belangrijker nog vind ik de aandachtsgebieden en leerpunten waarop ik mezelf kan ontwikkelen. Het DNA rapport biedt naast een eigen, uniek salesprofiel ook zeer constructieve tips die mij verder helpen commerciële kansen optimaal te benutten. Een rake zelfreflectie dus, die verder gaat dan een persoonlijk ontwikkelingsplan!</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/pauline_brinkman.jpg" alt="" />
+   </div>
+
+   <div class="name">Pauline Brinkman
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Als leider van een team is het voor mij belangrijk om jezelf goed te kennen. Hierdoor is het makkelijker om aan jezelf en met anderen te werken. Het InsightYou Sales profiel biedt verhelderende inzichten. Het is voor eenieder die vooruitgang wil in zijn persoonlijke ontwikkeling een goed meetpunt met een duidelijk onderbouwd rapport en eventuele verbeterpunten. Met goede tips om hiermee om te gaan.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/ivo_van_herwaarde.jpg" alt="" />
+   </div>
+
+   <div class="name">Ivo van Herwaarde
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Ik ben van nature een positief ingesteld persoon en werd dan ook erg enthousiast toen ik mijn DNA mocht afstaan voor het InsightYou Sales profiel. De uitkomsten hebben mij de focus gegeven om mij juist te concentreren op die punten waar mijn biologische talent en professionele ontwikkeling samenkomen. Zo kan ik waar ik van nature succesvol in ben nog verder uitbouwen tot een onderscheidend vermogen ten opzichte van andere sales professionals.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/john_houtman.jpg" alt="" />
+   </div>
+
+   <div class="name">John Houtman
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Mijn InsightYou Sales profiel heeft mij een waardevol inzicht gegeven in hoe het verschil kan worden gemaakt in sales. Daarnaast heb ik geleerd welke kerneigenschappen ik verder moet ontwikkelen om succesvol te blijven. De uitkomsten zijn fascinerend en blijken een goed handvat te bieden om de juiste vervolgstappen te definiëren. Mooi en vooral erg nuttig om deze combinatie van wetenschap en ervaring zo in de praktijk te zien!</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/adriaan_kom.jpg" alt="" />
+   </div>
+
+   <div class="name">Adriaan Kom
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Wij wilden onze teamleden een beter inzicht geven in hun eigen sales kwaliteiten. Daarom hebben wij ervoor gekozen iedereen een Sales profiel aan te bieden. Het is natuurlijk even spannend om met heel je team DNA af te staan. Tijdens de uitslag herkende de mensen zich al snel in bijvoorbeeld hun rol als hunter of farmer. Ook leverde het veel nieuwe, vaak prikkelende, inzichten die ervoor zorgden dat de teamleden veel meer van zichzelf lieten zien.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/joost_heijmenberg.jpg" alt="" />
+   </div>
+
+   <div class="name">Joost Heijmenberg
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+
+   '<div class="testi-container">
+      <blockquote>
+         <p>Bezoek het LinkedIn profiel van Daniel Federer"Het samenvoegen van meerdere bronnen - waaronder je DNA - om tot een salesprofiel te komen is uniek, spannend en fascinerend. Uiteraard ontken je in eerste instantie de aandachtspunten. Als je vervolgens de laatste gesprekken nog eens eerlijk overdenkt, dan klopt het simpelweg en moet je wel je voordeel doen met de resultaten. De database met profielen blijft groeien en je hebt dus blijvend voordeel met steeds nieuwe inzichten.</p>
+      </blockquote>
+   </div>
+
+   <div class="image">
+      <img src="img/client_pictures/daniel_federer.jpg" alt="" />
+   </div>
+
+   <div class="name">Daniel Federer
+      <br/>
+      <span class="label white radius">Manager &ndash; Charm Shop</span>
+      <a href="#" class="darkbluetext"> <i class="icon-linkedin"></i></a>
+   </div>',
+   );
+
+   function testi_selector() {
+      global $testimonials;
+      $random_keys = array_rand($testimonials, 2);
+      return array($testimonials[$random_keys[0]], $testimonials[$random_keys[1]]);
+   };
+
+   $testimonialsToEcho = testi_selector();
+
+?>
