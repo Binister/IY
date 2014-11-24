9866b62eaa939d375a666085e2792e90c358f7a4
Robin Spiegel <robinspiegel@gmail.com>
2014-11-24 12:00:12 +0100

    Change header to darkbluetext

--- Live site/cta_block.php
+++ Live site/cta_block.php
@@ -1,7 +1,7 @@
 <section class="mo-content bg-full">
  <div class="row">
     <div class="large-12 columns mo-animate text-center" data-animate="fadeInDown">
-       <h4>Klaar om het maximale uit jezelf te halen? Begin vandaag nog.</h4>
+       <h4 class="darkbluetext increase-lineheight">Klaar om het maximale uit jezelf te halen?<br>Begin vandaag nog<span class="pinktext">.</span></h4>
 
        <a class="button orangebutton radius large" href="<?php echo $page5 ?>">Start mijn ontwikkeling<i class="icon-chevron-right"></i></a>
     </div>
